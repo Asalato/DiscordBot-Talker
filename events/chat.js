2bef79845b294c2996cf06d6e64ce1d385dc3c10
Masaki Mita <mimimita3131@gmail.com>
2022-12-12 09:49:57 +0900

    Update chat.js

--- events/chat.js
+++ events/chat.js
@@ -25,11 +25,11 @@ module.exports = {
 
         const {ChatGPTAPI} = await import("chatgpt");
         const api = new ChatGPTAPI({sessionToken: sessionToken});
+        await api.ensureAuth();
         if (!await api.getIsAuthenticated()) {
             await message.reply("認証に失敗しました。再度`/set ${SessionID}`コマンドで登録してください");
             return;
         }
-        await api.ensureAuth();
 
         const conversationId = await TokenStore.getConversationId(lastId);
         const parentMessageId = await TokenStore.getParentMessageId(lastId);
